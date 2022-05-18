f9deaf0190e80054b59b009d92c3ecb09a7e6a92
Juliano <julianolamb@gmail.com>
2022-05-18 10:23:44 -0300

    commit 3

--- src/main/java/com/mycompany/lesson/Lesson.java
+++ src/main/java/com/mycompany/lesson/Lesson.java
@@ -13,6 +13,6 @@ public class Lesson {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Version 1.0");
+        System.out.println("Version 1.1");
     }
 }
